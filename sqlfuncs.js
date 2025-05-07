0a820498ec211aa6f33aed68705509549a4890eb
-g <-g>
2025-05-07 21:36:17 +0100

    Shifts

--- sqlfuncs.js
+++ sqlfuncs.js
@@ -1,85 +1,60 @@
-export const execute = async (db, sql, params = []) => {
-  if (params && params.length > 0) {
-    return new Promise((resolve, reject) => {
-      db.run(sql, params, (err) => {
-        if (err) reject(err);
-        resolve();
-      });
-    });
-  }
-  return new Promise((resolve, reject) => {
-    db.exec(sql, (err) => {
-      if (err) reject(err);
-      resolve();
-    });
-  });
-};
-
-export const fetchAll = async (db, sql, params) => {
-  return new Promise((resolve, reject) => {
-    db.all(sql, params, (err, rows) => {
-      if (err) reject(err);
-      resolve(rows);
-    });
-  });
-};
-
-export const fetchFirst = async (db, sql, params) => {
-  return new Promise((resolve, reject) => {
-    db.get(sql, params, (err, row) => {
-      if (err) reject(err);
-      resolve(row);
-    });
-  });
-};
-
-export const DeleteData = async (db,sql,params) => {
+import * as basefuncs from "./basesqlfuncs.js"
+//create
+export const createProject = async (db, project_name) => {
   try {
-    await execute(db, sql, params);
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-
-export const createProject = async (db,project_id,project_name) =>{
-  try {
-    await execute(db, `INSERT INTO projects 
-            VALUES(?,?)`,[project_id,project_name]);
+    await basefuncs.execute(
+      db,
+      `INSERT INTO projects 
+              VALUES(?,?)`,
+      [null, project_name]
+    );
   } catch (err) {
     console.log(err);
   }
 };
 
-export const createUser = async (db,userdata) =>{
+export const createRecipient = async (db, receiver_email) => {
   try {
-    const stored = [null,...userdata]
-    console.log(stored)
-    await execute(db,`INSERT INTO employee 
-        VALUES(?,?,?,?,?,?,?,?)`,stored);
+    await basefuncs.execute(db, `INSERT INTO receivers VALUES(?,?)`, [
+      null,
+      receiver_email,
+    ]);
   } catch (err) {
     console.log(err);
   }
 };
 
-
-export const deleteUser = async (db,id) =>{
+export const createUser = async (db,userdata) => {
   try {
-    await DeleteData(db,"DELETE FROM employee WHERE id=?",[id])
+    const stored = [null, ...userdata];
+    await basefuncs.execute(
+      db,
+      `INSERT INTO employees 
+          VALUES(?,?,?,?,?,?,?,?,?)`,
+      stored
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+//delete
+export const deleteUser = async (db, id) => {
+  try {
+    await basefuncs.DeleteData(db, "DELETE FROM employees WHERE id=?", [id]);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
-
-export const updateUserbyID = async (db,id,attribute,data) =>{
-  const allowedAttributes = ['name', 'email', 'password', 'role'];
-  if (!allowedAttributes.includes(attribute)) {
-    throw new Error('Invalid attribute name');
-  }
+//update
+export const updateUserPassword = async (db, email, newpassword) => {
   try {
-    await execute(db,"UPDATE employee SET ? = ? WHERE id=?",[attribute,data,id])
+    await execute(db, "UPDATE employees SET password = ? WHERE email=?", [
+      newpassword,
+      email,
+    ]);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
